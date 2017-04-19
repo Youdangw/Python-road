fe246e9416ca568b6d052239870f6eaeefd443c8
youdangw <2319839733@qq.com>
2017-04-19 14:17:35 +0800

    2017/4/19 函数的参数 例题

--- test1.py
+++ test1.py
@@ -20,3 +20,11 @@ print(enroll('Sarah', 'F'))
 
 print(enroll('Bob', 'M', '7'))
 
+def calc(*numbers):
+    sum = 0
+    for n in numbers:
+        sum = sum + n * n
+    return sum
+# *nums 表示把 nums 这个list的所有元素作为可变参数传进去；
+nums = [1,2,3]
+print(calc(*nums))
