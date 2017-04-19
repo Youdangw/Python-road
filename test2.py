fcdb09b0520d377c2612e3dd8c59af7625532afa
youdangw <2319839733@qq.com>
2017-04-19 16:23:39 +0800

    2017/4/17 递归函数 例题

---
+++ test2.py
@@ -0,0 +1,20 @@
+def fact(n):
+    return fact_iter(n, 1)
+
+def fact_iter(num, product):
+    if num == 1:
+        return product
+    return fact_iter(num - 1, num * product)
+
+print(fact(10))
+
+# 垃圾递归 操他妈真鸡巴难
+def move(n, a, b, c):
+    if(n == 1):
+        print(a, "->", c)
+        return
+    move(n - 1, a, c, b)
+    move(1, a, b, c)
+    move(n - 1, b, a, c)
+
+move(3,"a","b","c")
